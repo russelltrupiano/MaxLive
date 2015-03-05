443d5159cb0449f91d9f2ba3a889edfcadd368c3
russelltrupiano <russelltrupiano@gmail.com>
2015-03-04 21:30:35 -0500

    everything is looking good from web side

--- routes/max.js
+++ routes/max.js
@@ -9,7 +9,11 @@ router.get('/', function(req, res) {
 var osc = require('node-osc');
 var tempoClient = new osc.Client('127.0.0.1', 8080);
 var otherClient = new osc.Client('127.0.0.1', 8081);
+var otherClient2 = new osc.Client('127.0.0.1', 8082);
+var otherClient3 = new osc.Client('127.0.0.1', 8083);
+var otherClient4 = new osc.Client('127.0.0.1', 8084);
 var vocoderClient = new osc.Client('127.0.0.1', 8085);
+var midiClient = new osc.Client('127.0.0.1', 8086);
 
 router.post('/tempo', function(req, res) {
 
@@ -26,6 +30,12 @@ router.post('/other', function(req, res) {
 router.post('/vocoder', function(req, res) {
 
     vocoderClient.send(req.body.theText);
+    res.send(200);
+});
+
+router.post('/midi', function(req, res) {
+
+    midiClient.send(req.body.theText);
     res.send(200);
 });
 
